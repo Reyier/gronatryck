129849dfa3d8bacbf0383967be48c0ae00b5c364
Reyier <adamreyier@gmail.com>
2024-10-10 13:49:46 +0200

    update

--- my-app/src/pages/Register.js
+++ my-app/src/pages/Register.js
@@ -31,26 +31,98 @@ function Register({ onRegister }) {
       return; // Do not proceed with registration
     }
 
-    // Call the onRegister function if all fields are filled
-    onRegister(user);
+    // Save user data to localStorage
+    localStorage.setItem('registeredUser', JSON.stringify(user));
+
+    // Optionally call the onRegister function if passed as a prop
+    if (onRegister) {
+      onRegister(user);
+    }
+
+    // Redirect to login page after registration
+    navigate('/login');
   };
 
   return (
     <div>
       <h2>Registrera</h2>
       <form onSubmit={handleSubmit}>
-        <input type="email" name="email" placeholder="Email" value={user.email} onChange={handleChange} required /><br />
-        <input type="password" name="password" placeholder="Lösenord" value={user.password} onChange={handleChange} required /><br />
-        <input type="text" name="firstname" placeholder="Förnamn" value={user.firstname} onChange={handleChange} required /><br />
-        <input type="text" name="lastname" placeholder="Efternamn" value={user.lastname} onChange={handleChange} required /><br />
-        <input type="text" name="companyName" placeholder="Företagsnamn (valfritt)" value={user.companyName} onChange={handleChange} /><br />
-        <input type="text" name="street" placeholder="Gata" value={user.street} onChange={handleChange} required /><br />
-        <input type="text" name="city" placeholder="Stad" value={user.city} onChange={handleChange} required /><br />
-        <input type="text" name="country" placeholder="Land" value={user.country} onChange={handleChange} required /><br />
-        <input type="text" name="zipcode" placeholder="Postnummer" value={user.zipcode} onChange={handleChange} required /><br />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={user.email}
+          onChange={handleChange}
+          required
+        /><br />
+        <input
+          type="password"
+          name="password"
+          placeholder="Lösenord"
+          value={user.password}
+          onChange={handleChange}
+          required
+        /><br />
+        <input
+          type="text"
+          name="firstname"
+          placeholder="Förnamn"
+          value={user.firstname}
+          onChange={handleChange}
+          required
+        /><br />
+        <input
+          type="text"
+          name="lastname"
+          placeholder="Efternamn"
+          value={user.lastname}
+          onChange={handleChange}
+          required
+        /><br />
+        <input
+          type="text"
+          name="companyName"
+          placeholder="Företagsnamn (valfritt)"
+          value={user.companyName}
+          onChange={handleChange}
+        /><br />
+        <input
+          type="text"
+          name="street"
+          placeholder="Gata"
+          value={user.street}
+          onChange={handleChange}
+          required
+        /><br />
+        <input
+          type="text"
+          name="city"
+          placeholder="Stad"
+          value={user.city}
+          onChange={handleChange}
+          required
+        /><br />
+        <input
+          type="text"
+          name="country"
+          placeholder="Land"
+          value={user.country}
+          onChange={handleChange}
+          required
+        /><br />
+        <input
+          type="text"
+          name="zipcode"
+          placeholder="Postnummer"
+          value={user.zipcode}
+          onChange={handleChange}
+          required
+        /><br />
         <button type="submit">Registrera</button>
       </form>
-      <p onClick={() => navigate('/login')}>Har du redan ett konto? Logga in här.</p>
+      <p onClick={() => navigate('/login')}>
+        Har du redan ett konto? Logga in här.
+      </p>
     </div>
   );
 }

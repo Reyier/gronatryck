837262c832fbd50aeed528e7cec56b73a6e5df63
maxmatts <120714254+maxmatts@users.noreply.github.com>
2024-11-01 18:18:37 +0100

    ändrade kundcsae bilder

--- my-app/src/pages/Assortment.js
+++ my-app/src/pages/Assortment.js
@@ -20,39 +20,47 @@ export default function Sortiment() {
   // Definierar listan över populära kategorier
   const categorylist = [
     {
-      to: "/produkter/T-shirts", 
-      img: "/img/informative/Creator 2.0_Heather Haze_Duo_Front_Main_0.jpg",
+      to: "/produkter/T-shirts",
+      img: "/img/informative/Creator 2.0_Heather Haze_Duo_Front_Main_0-small.jpg",
       alt: "Cool tröja",
       label: "T-shirt",
     },
     {
-      to: "/produkter/Västar", 
-      img: "/img/informative/Liner_Desert Dust_Duo_Front_Main_0.jpg",
+      to: "/produkter/Västar",
+      img: "/img/informative/Liner_Desert Dust_Duo_Front_Main_0-small.jpg",
       alt: "Västar",
       label: "Västar",
     },
     {
-      to: "/produkter/Jackor", 
-      img: "/img/informative/Bomber 2.0_Black_Duo_Front_Main_0.jpg",
+      to: "/produkter/Jackor",
+      img: "/img/informative/Bomber 2.0_Black_Duo_Front_Main_0-small.jpg",
       alt: "Jackor",
       label: "Jackor",
     },
     {
-      to: "/produkter/Huvtröjor", 
-      img: "/img/informative/Connector 2.0_French Navy_Duo_Front_Main_0.jpg",
+      to: "/produkter/Huvtröjor",
+      img: "/img/informative/Connector 2.0_French Navy_Duo_Front_Main_0-small.jpg",
       alt: "Huvtröjor",
       label: "Huvtröjor",
     },
   ];
 
   return (
-    <div className="container" style={{ marginBlockStart: "10.8rem" }}> {/* Huvudbehållare för innehållet med margin */}
-      <NavList key="category-menu" links={categories} className="categories" /> {/* Renderar navigationslistan */}
-
+    <div className="container" style={{ marginBlockStart: "10.8rem" }}>
+      {" "}
+      {/* Huvudbehållare för innehållet med margin */}
+      <NavList
+        key="category-menu"
+        links={categories}
+        className="categories"
+      />{" "}
+      {/* Renderar navigationslistan */}
       <div className="nav-heading">
-        <h1 className="section-heading heading-3">Populära Kategori</h1> {/* Rubrik för sektionen med populära kategorier */}
+        <h1 className="section-heading heading-3">Populära Kategori</h1>{" "}
+        {/* Rubrik för sektionen med populära kategorier */}
       </div>
-      <PopularCategories categoryList={categorylist} /> {/* Renderar komponenten för populära kategorier */}
+      <PopularCategories categoryList={categorylist} />{" "}
+      {/* Renderar komponenten för populära kategorier */}
     </div>
   );
 }
